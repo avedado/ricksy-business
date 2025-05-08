bc6deb2c7772697aa14eb560c448e5575d35b88d
avedado <mariaalonsoteis@gmail.com>
2025-05-08 14:00:12 +0200

    feat: añadir métodos a la clase UfosPark

--- src/main/java/edu/estatuas/dispatchers/UfosPark.java
+++ src/main/java/edu/estatuas/dispatchers/UfosPark.java
@@ -1,4 +1,17 @@
 package edu.estatuas.dispatchers;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UfosPark {
+    private double fee;
+    private final Map<String, String> flota = new HashMap<String, String>();
+    UfosPark(){};
+
+    public void String add(){
+    }
+
+
+
+
 }
